3f211cb1e4eb65795a9037588aa6ae30b0f87af2
Aaron Evan-Browning <aaevan@gmail.com>
2019-03-08 22:14:28 -0500

    Added a switch for unix or windows style command line. (append --unix or --windows)

--- part_leveler.py
+++ part_leveler.py
@@ -9,8 +9,16 @@ argv = argv[argv.index("--") + 1:]  # get all args after "--"
 print(argv) 
 
 #folder_name = argv[0]
-folder_name = ' '.join(argv)
+folder_name = argv[0]
 counter = 0
+
+if '--windows' in argv:
+    slash_style = '\\'
+elif '--unix' in argv:
+    slash_style = '/'
+else:
+    print("Please choose a path style by appending --windows or --unix.")
+    exit()
 
 for filename in os.listdir(folder_name):
     if filename.endswith(".stl"):
@@ -30,8 +38,8 @@ for filename in os.listdir(folder_name):
 
         #import stl and assign properties
         #bpy.ops.import_mesh.stl(filepath=argv[0])
-        bpy.ops.import_mesh.stl(filepath = folder_name + '/' + filename) #unix
-        #bpy.ops.import_mesh.stl(filepath = folder_name + "\\" + filename) #windows
+        file_path = folder_name + slash_style + filename
+        bpy.ops.import_mesh.stl(filepath = file_path) #unix
         bpy.ops.object.origin_set(type='ORIGIN_CENTER_OF_VOLUME')
         bpy.context.object.location[0] = 0
         bpy.context.object.location[1] = 0
@@ -41,8 +49,7 @@ for filename in os.listdir(folder_name):
         bpy.context.scene.frame_current = 249
         bpy.ops.ptcache.bake_all(bake=False)
         #bpy.ops.export_mesh.stl(filepath = argv[0], use_selection = True)
-        bpy.ops.export_mesh.stl(filepath = folder_name + "/" + filename, use_selection = True) #unix
-        #bpy.ops.export_mesh.stl(filepath = folder_name + "\\" + filename, use_selection = True) #windows
+        bpy.ops.export_mesh.stl(filepath = file_path, use_selection = True) #unix
         bpy.ops.object.select_all(action='SELECT')
         bpy.ops.object.delete(use_global=False)
 print("Finished leveling {} parts.".format(counter))
