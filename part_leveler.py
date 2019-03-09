31735ba8819b1fc7cce4ce328ab48ce14aa36683
Aaron Evan-Browning <aaevan@gmail.com>
2019-03-08 22:42:04 -0500

    cleaned up part_leveler.py

--- part_leveler.py
+++ part_leveler.py
@@ -8,7 +8,6 @@ argv = argv[argv.index("--") + 1:]  # get all args after "--"
 
 print(argv) 
 
-#folder_name = argv[0]
 folder_name = argv[0]
 counter = 0
 
@@ -24,7 +23,7 @@ for filename in os.listdir(folder_name):
     if filename.endswith(".stl"):
         counter += 1
         print("({} of {}) processing {}...".format(counter, len(os.listdir(folder_name)), filename))
-        #delete starting scene
+        #for good measure, delete everything
         bpy.ops.object.select_all(action='SELECT')
         bpy.ops.object.delete(use_global=False)
 
@@ -37,19 +36,22 @@ for filename in os.listdir(folder_name):
         bpy.ops.object.select_all(action='DESELECT')
 
         #import stl and assign properties
-        #bpy.ops.import_mesh.stl(filepath=argv[0])
         file_path = folder_name + slash_style + filename
-        bpy.ops.import_mesh.stl(filepath = file_path) #unix
+        bpy.ops.import_mesh.stl(filepath = file_path)
         bpy.ops.object.origin_set(type='ORIGIN_CENTER_OF_VOLUME')
         bpy.context.object.location[0] = 0
         bpy.context.object.location[1] = 0
         bpy.ops.rigidbody.object_add()
         bpy.context.object.rigid_body.mass = 1000
-        #bpy.ops.screen.animation_play()
+
+        #simulate 249 frames of animation
         bpy.context.scene.frame_current = 249
         bpy.ops.ptcache.bake_all(bake=False)
-        #bpy.ops.export_mesh.stl(filepath = argv[0], use_selection = True)
-        bpy.ops.export_mesh.stl(filepath = file_path, use_selection = True) #unix
+
+        #export settled mesh in place
+        bpy.ops.export_mesh.stl(filepath = file_path, use_selection = True)
+
+        #delete everything again in preparation for the next stl
         bpy.ops.object.select_all(action='SELECT')
         bpy.ops.object.delete(use_global=False)
 print("Finished leveling {} parts.".format(counter))
